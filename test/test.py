6374a827a4ee13e0417c9fcf193a759291f8fb0b
Joel <joelalovera@gmail.com>
2014-12-15 20:21:51 -0300

    add tests

--- test/test.py
+++ test/test.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_elastiquery import elastic_query
 
 Base = declarative_base()
 
