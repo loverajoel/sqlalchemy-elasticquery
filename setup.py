6bf89e445cff07650a2255525c13d8a1539f5588
Jesus Roldan <xebuz18@gmail.com>
2014-12-21 20:04:04 -0300

    Better setup conf

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
-
 from setuptools import setup
+
 
 def read(*paths):
     """Build a file path from *paths* and return the contents."""
@@ -8,16 +8,26 @@ def read(*paths):
         return f.read()
 
 setup(
-    name='sqlalchemy_elasticquery',
+    name='sqlalchemy-elasticquery',
     version='0.0.1',
     description='Use ElasticSearch query search in SQLAlchemy.',
     url='https://github.com/loverajoel/sqlalchemy-elasticquery',
     license='MIT',
-    author='Lovera Joel',
+    author='Joel Lovera',
     author_email='joelalover@gmail.com',
     packages=['sqlalchemy_elasticquery'],
+    long_description=open('README.md').read(),
     include_package_data=True,
     install_requires=[
         'SQLAlchemy>=0.7.8',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins'
+        'Framework :: Flask',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database'
     ]
 )
